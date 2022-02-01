19ecdc2435064686d48146e4457b5690e057a4c0
chichafon <chichafonchichafon@gmail.com>
2022-02-01 15:31:37 +0400

    Initial Commit

---
+++ Apple Music GIT/Apple_Music_GITApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Music_GITApp.swift
+//  Apple Music GIT
+//
+//  Created by 111 on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Music_GITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
